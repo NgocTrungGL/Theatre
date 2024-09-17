f047d82bdbb0e54aeadce6a1bef8c7a91f0bf1fa
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-09-17 14:14:54 +0700

    fix hidden

--- src_react/movies_web/src/App.js
+++ src_react/movies_web/src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./pages/Header";
+import Banner from "./pages/Banner";
 
 function App() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <Banner />
+        </>
+    );
 }
 
 export default App;
--- src_react/movies_web/src/pages/Banner.jsx
+++ src_react/movies_web/src/pages/Banner.jsx
@@ -5,9 +5,9 @@ function Banner() {
     const [movies, setMovies] = useState([]);
     const fetchData = () => {
         fetch("http://localhost:3000/data/movieData.json")
-            .then((res) => res.json)
+            .then((res) => res.json())
             .then((data) => setMovies(data))
-            .catch((e) => console.log(e.massage));
+            .catch((e) => console.log(e.message));
     };
     useEffect(() => {
         fetchData();
@@ -15,38 +15,36 @@ function Banner() {
     return (
         <div className="banner">
             <div className="movie">
-                <img
-                    src={bgImg}
-                    alt="Background IMG"
-                    className="bgImg active"
-                />
-                <div className="container-fluid"></div>
-                <div className="row">
-                    <div className="col-lg-6 col-md-12">
-                        <div className="content">
-                            <img
-                                src=""
-                                alt="Movie Title"
-                                className="movie-title"
-                            />
-                            <h4>
-                                <span>Year</span>
-                                <span>
-                                    <i>age</i>
-                                </span>
-                                <span>length</span>
-                                <span>category</span>
-                            </h4>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur
-                                adipisicing elit. Similique, id ullam. Corrupti
-                                est non blanditiis temporibus fugiat obcaecati
-                                ex vitae libero, facere saepe delectus totam.
-                            </p>
-                            <div className="button">Buttton</div>
+                <img src={bgImg} alt="Background" className="bgImg" />
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-12">
+                            <div className="content">
+                                <img
+                                    src=""
+                                    alt="Movie-title"
+                                    className="movie-title"
+                                />
+                                <h4>
+                                    <span>Year</span>
+                                    <span>
+                                        <i>age</i>
+                                    </span>
+                                    <span>length</span>
+                                    <span>category</span>
+                                </h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur
+                                    adipisicing elit. At minus reprehenderit
+                                    nostrum. Iusto soluta quidem veritatis,
+                                    optio, reprehenderit fugiat ratione
+                                    cupiditate, molestias impedit quasi maiores.
+                                </p>
+                                <div className="button">Button</div>
+                            </div>
                         </div>
+                        <div className="col-lg-6 col-md-12"></div>
                     </div>
-                    <div className="col-lg-6 col-md-12"></div>
                 </div>
             </div>
         </div>
