fa5683f9b557ab691e950859f1b74b18c2b41cce
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-09-25 16:55:29 +0700

    fixed mapping data

--- src_react/movies_web/src/pages/Banner.jsx
+++ src_react/movies_web/src/pages/Banner.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./banner.css";
 
-import bgImg from "../img/bg-transformer.jpg";
 import MovieContent from "../components/MovieContent";
 import MovieDate from "../components/MovieDate";
 import PlayBtn from "../components/PlayBtn";
@@ -9,38 +8,50 @@ import MovieSwiper from "../components/MovieSwiper";
 
 function Banner() {
     const [movies, setMovies] = useState([]);
+
+    // Fetch data from the API
     const fetchData = () => {
         fetch("http://localhost:3000/data/movieData.json")
             .then((res) => res.json())
-            .then((data) => setMovies(data))
+            .then((data) => {
+                // Set the first movie as active initially
+                const updatedMovies = data.map((movie, index) => ({
+                    ...movie,
+                    active: index === 0, // Set first movie as active
+                }));
+                setMovies(updatedMovies);
+            })
             .catch((e) => console.log(e.message));
     };
+
     useEffect(() => {
         fetchData();
     }, []);
 
+    // Handle slide change and update active movie
     const handleSlideChange = (id) => {
-        const newMovies = movies.map((movie) => {
-            movie.active = false;
-            if (movie._id === id) {
-                movie.active = true;
-            }
-            return movie;
-        });
-        setMovies(newMovies);
+        // Create a new movie array with only the clicked movie set to active
+        const newMovies = movies.map((movie) => ({
+            ...movie,
+            active: movie._id === id, // Only set the movie with matching id as active
+        }));
+        setMovies(newMovies); // Update the movies state
     };
+
     return (
         <div className="banner">
-            {movies &&
-                movies.length > 0 &&
+            {movies.length > 0 &&
                 movies.map((movie) => (
-                    <div className="movie">
+                    <div
+                        className={`movie ${
+                            movie.active ? "active-movie" : ""
+                        }`}
+                        key={movie._id}
+                    >
                         <img
                             src={movie.bgImg}
                             alt="Background"
-                            className={`bgImg ${
-                                movie.active ? "active" : undefined
-                            }`}
+                            className={`bgImg ${movie.active ? "active" : ""}`} // Conditional class for active movie
                         />
                         <div className="container-fluid">
                             <div className="row">
@@ -56,7 +67,7 @@ function Banner() {
                     </div>
                 ))}
 
-            {movies && movies.length > 0 && (
+            {movies.length > 0 && (
                 <MovieSwiper slides={movies} slideChange={handleSlideChange} />
             )}
         </div>
