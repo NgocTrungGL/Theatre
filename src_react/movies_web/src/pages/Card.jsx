31b3bbda63c742b4588ebd3a4bc16e986f3e7514
Phuoc <phuoc11102004@gmail.com>
2024-09-29 00:25:06 +0700

    card.jsx

---
+++ src_react/movies_web/src/pages/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Card(movie) {
+    return (
+        <div className="col-lg-2 col-md-4 col-sm-6">
+            <div className="movie-card">
+                <img src={movie.previewImg} alt="Priview Image" className="img-fluid"/>
+                <p>
+                    {movie.length} | {movie.category}
+                </p>
+                <div className="content">
+                    <h4>{movie.title}</h4>
+                    <div className="card-icons"></div>
+                    <ion-icon name="add-outline"></ion-icon>
+                    <ion-icon name="play-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src_react/movies_web/src/pages/Schedule.jsx
+++ src_react/movies_web/src/pages/Schedule.jsx
@@ -1,9 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import './schedule.css';
+import Card from './Card';
 
 function Schedule(){
     const [data, setData] = useState([])
     const [movies, setMovies] = useState([])
+
+    const fetchData = () => {
+        fetch("http://localhost:3000/data/movieData.json")
+            .then((res) => res.json())
+            .then((data) => setData(data))
+            .catch((e) => console.log(e.message));
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    useEffect(() =>{
+        setMovies(data);
+    })
+
     return (
         <section id="schedule" className='schedule'>
             <div className='container-fluid' >
@@ -16,7 +33,9 @@ function Schedule(){
                     </div>
                 </div>
                 <div className="row mt-5">
-
+                    {movies && 
+                        movies.length > 0 && 
+                        movies.map(movie => (<Card key = {movie._id} movie = {movie} />))}
                 </div>
             </div>
         </section>
