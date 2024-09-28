57201523bb38a117e44e5ed5c8b3e3f024e54a1c
Phuoc <phuoc11102004@gmail.com>
2024-09-28 23:59:33 +0700

    schedule

--- src_react/movies_web/src/pages/Schedule.jsx
+++ src_react/movies_web/src/pages/Schedule.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './schedule.css';
 
 function Schedule(){
+    const [data, setData] = useState([])
+    const [movies, setMovies] = useState([])
     return (
         <section id="schedule" className='schedule'>
             <div className='container-fluid' >
                 <div className='row' >
                    <h4 className='section-title'>Opening this week </h4> 
+                </div>
+                <div className='row'>
+                    <div className='filters'>
+                        <p>Filters</p>
+                    </div>
+                </div>
+                <div className="row mt-5">
+
                 </div>
             </div>
         </section>
