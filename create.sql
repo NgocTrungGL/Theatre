6fb949f7bd8916611fc62f44197fc58e845605a2
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-11-18 22:04:32 +0700

    Create movie api

--- create.sql
+++ create.sql
@@ -12,7 +12,7 @@ CREATE TABLE Movies (
     date VARCHAR(50), -- Ngày phát hành
     ageLimit VARCHAR(10), -- Giới hạn độ tuổi (15+, 18+...)
     length VARCHAR(50), -- Thời lượng phim (vd: 2h 07min)
-    genre_id VARCHAR(50), -- Thể loại phim (vd: Adventure, Action...)
+    category VARCHAR(50), -- Thể loại phim (vd: Adventure, Action...)
     type ENUM('coming', 'released'), -- Trạng thái phim (sắp ra mắt hoặc đã phát hành)
     description TEXT, -- Mô tả nội dung phim
     active BOOLEAN DEFAULT FALSE -- Trạng thái phim có đang hoạt động hay không
@@ -36,16 +36,11 @@ CREATE TABLE Watchlist (
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
 );
-CREATE TABLE Genres (
-    genre_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
-);
-
--- Bảng Movie_Genres (Liên kết phim với thể loại)
-CREATE TABLE Movie_Genres (
-    movie_id INT,
-    genre_id INT,
-    PRIMARY KEY (movie_id, genre_id),
-    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
-    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
-);
+-- -- Bảng Movie_Genres (Liên kết phim với thể loại)
+-- CREATE TABLE Movie_Genres (
+--     movie_id INT,
+--     genre_id INT,
+--     PRIMARY KEY (movie_id, genre_id),
+--     FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
+--     FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
+-- );
